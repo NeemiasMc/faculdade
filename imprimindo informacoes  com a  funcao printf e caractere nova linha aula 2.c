5543d061268ac106c606bd83b5bc5609a70ed7cc
NeemiasMc <150401349+NeemiasMc@users.noreply.github.com>
2023-11-16 20:47:00 -0300

    Add files via upload
    aula 2

---
+++ imprimindo informacoes  com a  funcao printf e caractere nova linha aula 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+// caractere de escape pra salta uma linha -> \n
+int main() {
+
+// printf e uam funcao de saida
+	printf("\n-------------------------------\n");
+	printf(" \n1 - logar  2 - cadastrar 3- imprimir\n");
+	printf("\n-------------------------------\n");
+	
+	return 0;
+}
